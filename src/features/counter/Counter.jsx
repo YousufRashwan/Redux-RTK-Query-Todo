3f6f18bf099a41528b5dae08e43e268a10c8bb46
Yousuf Rashwan <89117787+YousufRashwan@users.noreply.github.com>
2024-07-06 11:06:06 +0300

    First Lesson: Basic Counter

---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+import { useState } from "react";
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  const addValue = Number(incrementAmount) || 0;
+  const resetAll = () => {
+    // Set the amount in (incrementBy) input to zero
+    setIncrementAmount(0);
+    // Set the main number to zero
+    dispatch(reset());
+  };
+  return (
+    <section>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>
+          Add Amount
+        </button>
+        <button onClick={resetAll}>Reset</button>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
